fac9948824816e52f854535cef005ed846ee274f
pbasalygo <p.basalygo@student.po.edu.pl>
2020-05-09 23:47:45 +0200

    date on employees_list fix

--- Frontend/src/app/shared/employee.model.ts
+++ Frontend/src/app/shared/employee.model.ts
@@ -2,7 +2,7 @@ export class Employee {
     constructor(public id: number,
                 public name: string,
                 public surname: string,
-                public joindateDate: Date,
+                public joindate: Date,
                 public phone: number,
                 public email: string,
                 public department: string) {}
