52cb6e64e8bdd13e6f9520d931e3295fe5085c81
Adam <adamo.b1997@gmail.com>
2020-05-20 17:27:28 +0200

    Added Entity Framework
    Changed Entity Framework version to 2.2, added connection string and dbContext.

--- Backend/PO.Project.Api/Startup.cs
+++ Backend/PO.Project.Api/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using PO.Project.Infrastructure.Persistence;
 
 namespace PO.Project.Api
 {
@@ -26,6 +28,9 @@ namespace PO.Project.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ProjectDbContext>(options => options
+                .UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSwaggerGen(c =>
             {
