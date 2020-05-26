54c2ee8d8e5fae12ea391897e6456dc821d8898c
Adam <adamo.b1997@gmail.com>
2020-05-26 17:21:53 +0200

    Update ProjectDbContext.cs

--- Backend/PO.Project.Infrastructure/Persistence/ProjectDbContext.cs
+++ Backend/PO.Project.Infrastructure/Persistence/ProjectDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PO.Project.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +10,12 @@ namespace PO.Project.Infrastructure.Persistence
     {
         public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options) { }
 
+        public virtual DbSet<CarPart> CarParts { get; set; }
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Owner> Owners { get; set; }
+        public virtual DbSet<Repair> Repairs { get; set; }
+        public virtual DbSet<Service> Services { get; set; }
+        public virtual DbSet<User> Users { get; set; }
     }
 }
